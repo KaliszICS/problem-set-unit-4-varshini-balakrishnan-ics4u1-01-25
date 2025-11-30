90bd2eb02cf31470b9e7b4ec6e9cf4f439545c54
Varshini Balakrishnan <varshini.balakrishnan@student.tdsb.on.ca>
2025-11-30 22:11:55 +0000

    update

--- src/main/java/ExampleClass.java
+++ src/main/java/ExampleClass.java
@@ -1,4 +1,25 @@
 
-class ExampleClass {
+//option 3 (Still deciding)
+public class ExampleClass {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        Card c = new Card("Queen", "Hearts", 12);
+        System.out.println(c);
+    }
 	
+}
+
+class Card {
+private String name;
+private String suit;
+private int value;
+
+Card(String n, String s, int v) {
+    name = n;
+    suit = s;
+    value = v;
+}
+public String tostring() {
+    return "";
+}
 }
