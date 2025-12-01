72e7beb85657c54fd2dd0ebcd908e73f7483064a
Varshini Balakrishnan <varshini.balakrishnan@student.tdsb.on.ca>
2025-12-01 03:30:41 +0000

    update

--- src/main/java/ExampleClass.java
+++ src/main/java/ExampleClass.java
@@ -4,7 +4,14 @@ public class ExampleClass {
     public static void main(String[] args) {
         System.out.println("Test");
         Card c = new Card("Queen", "Hearts", 12);
-        System.out.println(c);
+        Card c2 = new Card("Queen", "Hearts", 12);
+
+        System.out.println(c); //queen of hearts
+        System.out.println(c.getName()); //queen
+        System.out.println(c.getSuit()); //hearts
+        System.out.println(c.getValue()); //12
+        System.out.println(c.equals(c2)); //false 
+        System.out.println(c.equals(null)); //=false
     }
 	
 }
@@ -19,7 +26,29 @@ Card(String n, String s, int v) {
     suit = s;
     value = v;
 }
-public String tostring() {
-    return "";
+public String toString() {
+    return name + " of " + suit;
+}
+public String getName() {
+    return name;
+}
+public String getSuit() {
+    return suit;
+}
+public int getValue() {
+    return value;
+}
+public boolean equals(Card other) {
+    if (other == null) {
+        return false;
+    }
+    boolean sameName = this.getName().equals(other.getName());
+    boolean sameSuit = this.getSuit().equals(other.getSuit());
+    boolean sameValue = this.getValue() == (other.getValue());
+    if (sameName && sameSuit && sameValue) {
+        return true;
+    } else {
+        return false;
+    }
 }
 }
